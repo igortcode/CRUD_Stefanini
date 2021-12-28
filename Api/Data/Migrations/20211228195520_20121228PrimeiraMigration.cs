8dc80015bf64a183c8956488bca054ce20fbc03e
IgorTorres <itorres@provertec.com.br>
2021-12-28 17:00:21 -0300

    Primeira Migration

---
+++ Api/Data/Migrations/20211228195520_20121228PrimeiraMigration.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class _20121228PrimeiraMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonPhone",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    PersonId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonPhone", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PersonPhone_Person_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Person",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PhoneNumberType",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    PersonPhoneId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PhoneNumberType", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PhoneNumberType_PersonPhone_PersonPhoneId",
+                        column: x => x.PersonPhoneId,
+                        principalTable: "PersonPhone",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonPhone_PersonId",
+                table: "PersonPhone",
+                column: "PersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PhoneNumberType_PersonPhoneId",
+                table: "PhoneNumberType",
+                column: "PersonPhoneId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PhoneNumberType");
+
+            migrationBuilder.DropTable(
+                name: "PersonPhone");
+
+            migrationBuilder.DropTable(
+                name: "Person");
+        }
+    }
+}

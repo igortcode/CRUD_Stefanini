1b5b0face65ce112b101f8ea28aea4bc4744a7c6
IgorTorres <itorres@provertec.com.br>
2021-12-28 09:18:26 -0300

    Estruturando o projeto

---
+++ Api/Business/Entities/Person.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Business.Entities
+{
+    public class Person : Entity
+    {
+        public string Name { get; set; }
+        public ICollection<PersonPhone> Phones { get; set; }
+    }
+}
---
+++ Api/Business/Entities/PersonPhone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    public class PersonPhone : Entity
+    {
+        public string PhoneNumber { get; set; }
+
+        public int PhoneNumberTypeID { get; set; }
+
+        public Person Person { get; set; }
+
+        public PhoneNumberType PhoneNumberType { get; set; }
+    }
+}
